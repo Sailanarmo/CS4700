498f9000a5f4bccfacf31fec2dcf9bde86c5c93e
Sailanarmo <rlramirez12@gmail.com>
2017-09-30 18:26:54 -0600

    Finished assignment.

--- Assignment3/look.pl
+++ Assignment3/look.pl
@@ -3,12 +3,21 @@
 connected(X,Y):-door(X,Y).
 connected(X,Y):-door(Y,X).
 
-shortDesc(Place):-short_desc(Place,Desc),write(Desc),nl,!.
 
-listConnected(Place):-connected(Place,Other), name(Other,Name),write(Name),write(" - "),shortDesc(Other),fail.
+shortDesc(Place):-short_desc(Place,Desc),name(Place,Name),write(Name),write(" - "),write(Desc),nl,!.
+
+longDesc(Object):-long_desc(Object,Desc),name(Object,Name),write(Name),write(" - "),write(Desc),nl,!.
+
+listConnected(Place):-connected(Place,Other),shortDesc(Other),fail.
 listConnected(_).
 
-listItems(Place):-location(X,Place),name(X,Name),write("a "),write(Name),write(" - "),shortDesc(X),fail.
+listItems(Place):-location(X,Place),shortDesc(X),fail.
 listItems(_).
 
-lookAtRoom(Room):-name(Room,Name),nl,write("You are in "),write(Name),nl,nl,write("You see around you: "),nl,listItems(Room),nl,write("It is Connected to"),nl,listConnected(Room),!.
+
+%========================================================
+
+look(Room):-name(Room,Name),nl,write("You are in "),write(Name),nl,nl,write("You see around you: "),nl,listItems(Room),nl,write("It is Connected to"),nl,listConnected(Room),!.
+
+%if search is a container, then list the items, else it is an object and should return a desc.
+search(Item):-(container(Item) -> listItems(Item) ; longDesc(Item)),!.
